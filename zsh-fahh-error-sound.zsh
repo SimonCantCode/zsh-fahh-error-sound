91a0b9b0f19cb32e859dd8493bd414d7076a98e3
Simon <simondepomian@gmail.com>
2026-03-04 15:21:33 +0100

    ignore keyboard interrupt and comment on bash support

--- zsh-fahh-error-sound.zsh
+++ zsh-fahh-error-sound.zsh
@@ -1,7 +1,11 @@
 # Fahh!
 # i still dont know if i should spell it "fahh" or "fah" xd
 function fahh() {
-	if [[ $? -ne 0 ]]; then
+	local rc=$? # no spaces allowed between the = apparently lol
+	if [[ $rc -eq 130 ]]; then # To ignore keyboard interrupt.
+		return
+	fi
+	if [[ $rc -ne 0 ]]; then
 		( ffplay -nodisp -autoexit .config/zsh/plugins/zsh-fahh-error-sound/fah.mp3 > /dev/null 2>&1 &) > /dev/null
 	fi
 }
@@ -10,3 +14,4 @@ function fahh() {
 precmd(){
 	fahh
 }
+# For bash support, use PROMPT_COMMAND='fahh' instead of precmd.
